30ffcfc1e865cef859122475a51e0c6e4729ff5a
gcontrerascaneo <34114895+gcontrerascaneo@users.noreply.github.com>
2017-12-01 02:36:48 -0300

    Add files via upload

---
+++ Mover.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/env python
+
+import nxt.locator
+from nxt.motor import *
+
+def spin_around(R2D2):
+    m_left = Motor(R2D2, PORT_C)
+    m_left.run(100)
+    m_right = Motor(R2D2, PORT_B)
+    m_right.run(100)
+    
+    m_left.idle()
+    m_right.idle()
+
+R2D2 = nxt.locator.find_one_brick()
+spin_around(R2D2)
